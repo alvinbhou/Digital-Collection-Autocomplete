400d4fe80c6d80c4141b4131e5fdd0eea77a6ce5
Alvin <alvinbhou@gmail.com>
2018-02-21 23:23:45 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Digital-Archive-Autocomplete
+協助國立歷史博物館「數位典藏完備計畫」之文物計畫填寫自動化
---
+++ script_big5.py
@@ -0,0 +1,39 @@
+import csv
+import re, sys
+
+if len(sys.argv) != 4:
+    print('[Usage]: python script_big5.py [info_path] [csv_path] [result_path]')
+    exit(1)
+content_path = sys.argv[1]
+csv_path = sys.argv[2]
+result_path = sys.argv[3]
+
+d = {}
+with open(content_path, 'r', encoding = 'big5') as f:
+    lines = f.readlines()
+    for line in lines:
+        if line == '\n' or len(line) < 3 :
+            continue
+        # extract_num = line[:7]
+        # num =  re.findall('\d+', extract_num )
+        num = line[0:5]
+        content = line[5:].rstrip()
+        d[num] = content
+# print(d)
+data_csv = []
+with open(csv_path, 'r', encoding = 'big5') as f:
+    re = csv.reader(f)
+    for idx, r in enumerate(re):
+        # skip first three rows
+        if(idx <= 2 or len(r) < 3):
+            continue
+        target_idx = r[0]
+        # 14: content index 
+        if target_idx in d:
+            nhm_header = "國立歷史博物館藏"
+            artefact_name = r[4]
+            r[14] = nhm_header + '《' + artefact_name + '》（' + target_idx + "）" +  d[target_idx]
+        data_csv.append(r)    
+
+writer = csv.writer(open('result.csv', 'w', encoding = 'big5', newline=''))
+writer.writerows(data_csv)
